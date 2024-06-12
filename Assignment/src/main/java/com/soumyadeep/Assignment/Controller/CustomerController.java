443f08ce13b1e004efb6df678933a1292390411e
Soumyadeep20 <111590530+Soumyadeep20@users.noreply.github.com>
2024-06-13 01:20:25 +0530

    DONE CustomerController.java

--- Assignment/src/main/java/com/soumyadeep/Assignment/Controller/CustomerController.java
+++ Assignment/src/main/java/com/soumyadeep/Assignment/Controller/CustomerController.java
@@ -1,41 +1,63 @@
 package com.soumyadeep.Assignment.Controller;
 
 import ch.qos.logback.core.model.Model;
+import com.soumyadeep.Assignment.Model.Customer;
 import com.soumyadeep.Assignment.Model.CustomerDTO;
 import com.soumyadeep.Assignment.Service.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
+//To create new Customer
 @RequestMapping("/customer")
 public class CustomerController {
 
    @Autowired
     CustomerService customerService;
+   @CrossOrigin(origins="http://localhost:8080/customer/save")
     @PostMapping("/save")
-    public String  save( @RequestBody CustomerDTO customer)
+    public ResponseEntity<Customer> save(@RequestBody CustomerDTO customer)
     {
-            System.out.println(customer);
-             customerService.save(customer);
-             return "done";
+        System.out.println(customer);
+        return customerService.save(customer);
+
 
 
     }
-    @GetMapping("/xyz")
-    public String check()
+    @GetMapping("/getById")
+    public ResponseEntity<Customer> fidByID(@RequestParam String id)
     {
-        return " valid";
+        return customerService.fidByID(id);
     }
+    @GetMapping("/allCustomerList")
+    public ResponseEntity<List<Customer>> getall()
+    {
+        return customerService.getall();
+    }
+
    @DeleteMapping("/delete")
-   public void delete(@RequestParam UUID id)
+   public void delete(@RequestParam String id)
     {
         customerService.delete(id);
+    }
+   //To update customer
+    @PutMapping("/update")
+    public ResponseEntity<String> update(@RequestBody Customer old)
+    {
+        return customerService.update(old);
+    }
+    // to Searching based on parameter and value
+    @GetMapping("/search")
+    public ResponseEntity<List<Customer>> search(@RequestParam String searchBy, @RequestParam String value)
+    {
+        return customerService.search(searchBy,value);
     }
 }
